be275463478f31d7b4d01d965ffbaaec3dcfa162
Blake Lamb <blake.lamb31@gmail.com>
2019-05-27 20:07:34 -0600

    Finished

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -1,4 +1,25 @@
-let books = []
+let books = [
+    // {
+    //     "title": "Harry Potter 7",
+    //     "author": "J.K. Rowling",
+    //     "id": 1
+    // },
+    // {
+    //     "title": "Harry Potter 6",
+    //     "author": "J.K. Rowling",
+    //     "id": 2
+    // },
+    // {
+    //     "title": "Harry Potter 5",
+    //     "author": "J.K. Rowling",
+    //     "id": 3
+    // },
+    // {
+    //     "title": "Harry Potter 4",
+    //     "author": "J.K. Rowling",
+    //     "id": 4
+    // }
+]
 
 let id = 1
 
@@ -16,5 +37,36 @@ module.exports = {
         books.push(newBook)
 
         res.send(books)
+    },
+    update: (req, res) => {
+        let {title, author} = req.body
+        let {id} = req.params
+
+        let editId = id
+        let editIndex = books.findIndex( (book) => +book.id === +editId)
+
+
+        let book = books[editIndex]
+
+        console.log(1111111, title, 2222222222, author)
+
+        books[editIndex] = {
+            title: title || book.title,
+            author: author || book.author,
+            id: book.id
+        }
+        
+        res.send(books)
+        
+    },
+    delete: (req, res) => {
+        let deleteId = req.params.id
+
+        let deleteIndex = books.findIndex( book => +book.id === +deleteId)
+
+        books.splice(deleteIndex, 1)
+
+        res.send(books)
+
     }
 }
--- server/index.js
+++ server/index.js
@@ -4,10 +4,14 @@ const port = 4000
 
 let bc = require('./controllers/books_controller')
 
+app.use(express.static(__dirname + "/../build"))
+
 app.use(express.json())
 
 app.get('',(req, res) => res.send("<h1>Welcome to the Books HomePage</h1>") )
 app.get('/api/books', bc.read)
 app.post('/api/books', bc.create)
+app.put('/api/books/:id', bc.update)
+app.delete('/api/books/:id', bc.delete)
 
 app.listen(port, () => console.log(`I am listening on Port: ${port}`))
